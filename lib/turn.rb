298d0ec7fad7bcb2a18478fd6eb92b240a694a26
Antonio Jackson <antoniojackson@gmail.com>
2020-03-19 20:46:12 -0600

    Reordering set_turn_type for :mutually_assured_destruction functionality

---
+++ lib/turn.rb
@@ -0,0 +1,59 @@
+require 'pry'
+
+class Turn
+
+  attr_reader :player1, :player2, :spoils_of_war, :type
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+    set_turn_type
+  end
+
+  def set_turn_type
+    if @player1.deck.cards[0].rank != @player2.deck.cards[0].rank
+      @type = :basic
+    elsif (@player1.deck.cards[0].rank == @player2.deck.cards[0].rank) && (@player1.deck.cards[2].rank == @player2.deck.cards[2].rank)
+      @type = :mutually_assured_destruction
+    elsif @player1.deck.cards[0].rank == @player2.deck.cards[0].rank
+      @type = :war
+    end
+  end
+
+  def winner
+    if @type == :basic
+      if @player1.deck.cards[0].rank >  @player2.deck.cards[0].rank
+        @player1
+      else
+        @player2
+      end
+    elsif @type == :war
+      if @player1.deck.cards[2].rank > @player2.deck.cards[2].rank
+        @player1
+      else
+        @player2
+      end
+    elsif @type == :mutually_assured_destruction
+      p "No Winner"
+    end
+  end
+
+  def pile_cards
+    if @type == :basic
+      @spoils_of_war << @player1.deck.cards.shift
+      @spoils_of_war << @player2.deck.cards.shift
+      @spoils_of_war
+    elsif @type == :war
+      @spoils_of_war = @player1.deck.cards.shift(3) + @player2.deck.cards.shift(3)
+    elsif @type == :mutually_assured_destruction
+      @player1.deck.cards.shift(3)
+      @player2.deck.cards.shift(3)
+    end
+  end
+
+  def award_spoils(winner)
+    winner.deck.cards.push(*@spoils_of_war)
+    @spoils_of_war =[]
+  end
+end
